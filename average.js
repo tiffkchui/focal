e422e9c9107264f3cd00cf56f880a87956bf5f94
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-04 20:40:39 -0700

    Update average.js

--- average.js
+++ average.js
@@ -2,9 +2,25 @@ function average(list) {
   var sum = 0;
 
   for (var num of list) {
-    sum ++ num;
+    sum; ++ num;
   }
 
   return sum / list.length;
 
 console.log(average([3, 5, 7]));
+
+//new code
+//put "="
+const average = function(list) {
+  let sum = 0
+
+  for (var num of list) {
+    //changed from ++ to +=
+    sum += num;
+    
+  }
+
+  return sum / list.length;
+
+}};
+  console.log(average([3, 5, 7]));
