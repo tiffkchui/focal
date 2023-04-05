ccf0f2e810e9ef12a8050577e0a4907712920905
tiffkchui <124648372+tiffkchui@users.noreply.github.com>
2023-04-04 20:31:46 -0700

    edited code

--- reverse.js
+++ reverse.js
@@ -6,3 +6,14 @@ if (input)
 function reverse(original) {
   return orignal.split('').reverseList().join('');
 }
+
+// new code
+var input = process.argv[2];
+
+function reverse(original) {
+  return original.split("").reverse().join("");
+}
+
+if (input){
+  console.log(reverse(input));
+}
